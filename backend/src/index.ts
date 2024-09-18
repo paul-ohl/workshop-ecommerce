abdd2c984c322210365cef1a298e17eb9c6f9c6d
Paul OHL <paul.lv.ohl@gmail.com>
2024-09-18 11:40:28 +0200

    fix: use mongo:jammy and removed useless code

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -7,7 +7,7 @@ dotenv.config();
 // Valider que les variables d'environnement sont présentes
 const mongoUri = process.env.MONGO_URI;
 const port = process.env.PORT;
-if (!mongoUri) { 
+if (!mongoUri) {
   throw new Error("MONGO_URI is not defined in the .env file");
 }
 if (!port) {
@@ -17,7 +17,6 @@ if (!port) {
 const app: Express = express();
 app.use(express.json());
 
-// Fonction pour se connecter à MongoDB
 const connectToDatabase = async () => {
   try {
     await mongoose.connect(mongoUri);
@@ -28,48 +27,9 @@ const connectToDatabase = async () => {
   }
 };
 
-// Définition du modèle d'utilisateur
-const mongSchema = new mongoose.Schema({
-  nom: String,
-  email: String,
-  age: Number,
-});
-
-const Mong = mongoose.model('Mong', mongSchema);
-
-// Fonction pour ajouter un utilisateur
-async function addMong(mongData: { nom: string; email: string; age: number }) {
-  try {
-    const mong = new Mong(mongData);
-    const savedMong = await mong.save();
-    console.log('Utilisateur ajouté avec succès:', savedMong);
-  } catch (err) {
-    console.error('Erreur lors de l\'ajout de l\'utilisateur:', err);
-  }
-}
-
-// Fonction pour récupérer et afficher tous les utilisateurs
-async function getAllMongs() {
-  try {
-    const mongs = await Mong.find();  // Récupère tous les utilisateurs
-    console.log('Liste des utilisateurs :', mongs);
-  } catch (err) {
-    console.error('Erreur lors de la récupération des utilisateurs:', err);
-  }
-}
-
-// Exemple d'utilisateur à ajouter
-const nouvelUtilisateur = {
-  nom: 'Jean3',
-  email: '3jean@example.com',
-  age: 30
-};
-
 // Connexion à MongoDB et ajout de l'utilisateur après la connexion
 const startApp = async () => {
   await connectToDatabase();  // Connexion à MongoDB
-  await addMong(nouvelUtilisateur);  // Ajout de l'utilisateur après la connexion
-  await getAllMongs();  // Affiche tous les utilisateurs après l'ajout
 
   app.listen(port, () => {
     console.log(`[server]: Server is running at http://localhost:${port}`);

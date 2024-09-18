70f94e505b553905d9a21d37a8307e032bd3bcc9
Paul OHL <paul.lv.ohl@gmail.com>
2024-09-18 16:48:11 +0200

    fix: Changed findOne to find

--- backend/src/services/config-service.ts
+++ backend/src/services/config-service.ts
@@ -2,6 +2,6 @@ import ConfigModel from "../models/config.model";
 
 export class ConfigService {
 	public async getAll() {
-		return await ConfigModel.findOne();
+		return await ConfigModel.find();
 	}
 }
